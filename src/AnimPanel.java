df81cf6830be3a2f604752ed70e1ab8881b82675
276720 <276720@student.pwr.edu.pl>
2024-01-08 16:41:30 +0100

    second commit

--- src/AnimPanel.java
+++ src/AnimPanel.java
@@ -1,11 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class AnimPanel extends JPanel {
+public class AnimPanel extends JPanel implements ActionListener {
     Prostokat prostokat;
+    private int delay = 70;
+
+    private Timer timer;
     public AnimPanel(){
         setBackground(Color.WHITE);
         setBounds(7, 11, 422, 219);
+        timer = new Timer(delay, this);
+        prostokat = new Prostokat();
+        timer.addActionListener(prostokat);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
 
     }
 }
--- src/Prostokat.java
+++ src/Prostokat.java
@@ -1,13 +1,26 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
 
 import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseListener;
 
-public class Prostokat  implements Runnable{
+public class Prostokat  implements Runnable, ActionListener {
+
     private int startX, startY, endX, endY, width, height, x, y;
+    private int delay = 70;
     private Component component;
+    protected Area area;
+    protected Shape shape;
+    protected AffineTransform aft;
+    private Point initialPosition;
     public Prostokat(){
+        aft = new AffineTransform();
+        area = new Area(shape);
+
         this.component = component;
         setDrawing();
     }
@@ -15,14 +28,12 @@ public class Prostokat  implements Runnable{
         addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                //super.mousePressed(e);
                 startX = e.getX();
                 startY = e.getY();
             }
 
             @Override
             public void mouseReleased(MouseEvent e) {
-                //super.mouseReleased(e);
                 endX = e.getX();
                 endY = e.getY();
 
@@ -42,6 +53,38 @@ public class Prostokat  implements Runnable{
 
     @Override
     public void run() {
+        //initialPosition = ;
+        aft.translate(initialPosition.getX(), initialPosition.getY());
+        area.transform(aft);
+        shape = area;
+        while (true) {
+            shape = nextFrame();
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+    public Shape nextFrame(){
+        area = new Area(area);
+        aft = new AffineTransform();
+        Rectangle bounds = area.getBounds();
+        int cx = bounds.x + bounds.width / 2;
+        int cy = bounds.y + bounds.height / 2;
+        if (cx > width){
+            cx = (int) initialPosition.getX() + bounds.width / 2;
+            cy = (int) initialPosition.getY() + bounds.height / 2;
+            area = new Area(new Rectangle(cx - bounds.width / 2, cy - bounds.height / 2, bounds.width, bounds.height));
+        }
+        aft.translate(cx, cy);
+        aft.translate(-cx, -cy);
+
+        area.transform(aft);
+        return area;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
 
     }
 }
